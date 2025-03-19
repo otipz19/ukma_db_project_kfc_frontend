56c2e927529c6478ec2f61bbac3a1eb783415ead
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-03-19 22:29:37 +0200

    Remove bootstrap and add registration page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'registration'
+  },
+  {
+    path: 'registration',
+    loadComponent: () => import('./domains/registration/view/page/registration-page.component').then(r => r.RegistrationPageComponent)
+  }
+];
---
+++ src/app/domains/registration/view/page/registration-page.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-registration-page',
+  standalone: true,
+  templateUrl: 'registration-page.component.html'
+})
+export class RegistrationPageComponent {
+
+}
