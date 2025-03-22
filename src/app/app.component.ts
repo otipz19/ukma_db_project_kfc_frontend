39005b9b749e01d09f7b40502dd8c2255228748a
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-03-22 23:42:15 +0200

    Update page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MatIconModule, MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet, MatIconModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kfc_frontend';
+
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'burger',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/burger.svg')
+    );
+  }
+
 }
--- src/app/domains/registration/view/page/registration-page.component.ts
+++ src/app/domains/registration/view/page/registration-page.component.ts
@@ -1,13 +1,17 @@
 import {Component} from "@angular/core";
 import {RegistrationFormComponent} from "../components/registration-form/registration-form.component";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
-    selector: 'app-registration-page',
-    templateUrl: 'registration-page.component.html',
-    imports: [
-        RegistrationFormComponent
-    ],
-    styleUrl: 'registration-page.component.scss'
+  selector: 'app-registration-page',
+  templateUrl: 'registration-page.component.html',
+  imports: [
+    RegistrationFormComponent,
+    MatToolbar,
+    MatIcon,
+  ],
+  styleUrl: 'registration-page.component.scss'
 })
 export class RegistrationPageComponent {
 
