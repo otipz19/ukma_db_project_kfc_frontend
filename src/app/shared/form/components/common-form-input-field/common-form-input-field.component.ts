58a4bbd71ee05f9ea0e1b5c5bf29f5433755bcf5
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-03-23 15:03:25 +0200

    Add icon suffixes to common-form-input-field

--- src/app/shared/form/components/common-form-input-field/common-form-input-field.component.ts
+++ src/app/shared/form/components/common-form-input-field/common-form-input-field.component.ts
@@ -1,8 +1,9 @@
-import {Component, input} from "@angular/core";
+import {Component, computed, input} from "@angular/core";
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
 import {ErrorMessagePipe} from "../../pipes/error-message.pipe";
 import {MatFormField, MatInput, MatLabel, MatPrefix, MatSuffix} from "@angular/material/input";
 import {MatError, MatHint} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
 
 export type FormInputFieldType =
   | "email"
@@ -23,7 +24,8 @@ export type FormInputFieldType =
     ReactiveFormsModule,
     MatFormField,
     MatPrefix,
-    MatSuffix
+    MatSuffix,
+    MatIcon
   ],
   templateUrl: 'common-form-input-field.component.html',
   styleUrl: 'common-form-input-field.component.scss'
@@ -39,4 +41,23 @@ export class CommonFormInputFieldComponent {
 
   readonly $textPrefix = input<string | undefined>(undefined, {alias: 'textPrefix'});
   readonly $textSuffix = input<string | undefined>(undefined, {alias: 'textSuffix'});
+
+  readonly $iconSuffix = input<string | undefined>(undefined, {alias: 'iconSuffix'});
+
+  protected readonly $computedIconSuffix = computed(() => {
+    if (this.$iconSuffix()) {
+      return this.$iconSuffix();
+    }
+
+    if (this.$inputType() in INPUT_TYPE_TO_ICON) {
+      return INPUT_TYPE_TO_ICON[this.$inputType()];
+    }
+
+    return undefined;
+  });
 }
+
+const INPUT_TYPE_TO_ICON: Partial<Record<FormInputFieldType, string>> = {
+  email: 'email',
+  tel: 'phone',
+} as const;
