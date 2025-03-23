ed46e42a1b0aeac0ad4040796e9efce525e33375
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-03-23 14:52:53 +0200

    Add login form

--- src/app/domains/login/view/components/login-form/login-form.component.ts
+++ src/app/domains/login/view/components/login-form/login-form.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {
+  MatCard,
+  MatCardActions, MatCardContent, MatCardFooter,
+  MatCardHeader,
+  MatCardTitle
+} from "@angular/material/card";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {emailValidator} from "../../../../../shared/form/validators/email.validator";
+import {
+  CommonFormInputFieldComponent
+} from "../../../../../shared/form/components/common-form-input-field/common-form-input-field.component";
+import {
+  CommonFormPasswordFieldComponent
+} from "../../../../../shared/form/components/common-form-password-field/common-form-password-field.component";
+import {MatAnchor, MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-login-form',
   imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatCardActions,
+    ReactiveFormsModule,
+    CommonFormInputFieldComponent,
+    CommonFormPasswordFieldComponent,
+    MatButton,
+    MatIcon,
+    MatCardFooter,
+    MatAnchor
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  private readonly fb = inject(FormBuilder).nonNullable;
 
+  protected readonly form = this.fb.group({
+    email: this.fb.control('', [Validators.required, emailValidator()]),
+    password: this.fb.control('', [Validators.required, Validators.maxLength(64)])
+  });
 }
