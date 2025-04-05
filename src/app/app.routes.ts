769719a1d3b852937be652cc988843bc9eaf7a17
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-04-05 21:50:21 +0300

    Create new layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,35 @@
 import {Routes} from '@angular/router';
+import {MainLayoutComponent} from "./layouts/main/main-layout.component";
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'auth',
     pathMatch: 'full'
   },
   {
     path: '',
-    loadComponent: () => import('./core/view/unauthorized-layout/unauthorized-layout.component').then(r => r.UnauthorizedLayoutComponent),
+    component: MainLayoutComponent,
     children: [
       {
-        path: 'registration',
-        loadComponent: () => import('./domains/registration/view/components/registration-form/registration-form.component').then(r => r.RegistrationFormComponent)
+        path: 'auth',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
       },
       {
-        path: 'login',
-        loadComponent: () => import('./domains/login/view/components/login-form/login-form.component').then(r => r.LoginFormComponent)
-      }
+        path: 'auth',
+        loadComponent: () => import('./layouts/auth/auth-layout.component').then(r => r.AuthLayoutComponent),
+        children: [
+          {
+            path: 'registration',
+            loadComponent: () => import('./domains/registration/view/components/registration-form/registration-form.component').then(r => r.RegistrationFormComponent)
+          },
+          {
+            path: 'login',
+            loadComponent: () => import('./domains/login/view/components/login-form/login-form.component').then(r => r.LoginFormComponent)
+          }
+        ]
+      },
     ]
-  },
+  }
 ];
---
+++ src/app/layouts/main/main-layout.component.ts
@@ -0,0 +1,71 @@
+import {Component, computed, signal} from '@angular/core';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatToolbar} from "@angular/material/toolbar";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+
+type SidenavLinkModel = {
+  routerLink: string[],
+  icon: string,
+  label: string
+};
+
+@Component({
+  selector: 'app-admin-layout',
+  imports: [
+    MatSidenavContainer,
+    MatIcon,
+    MatToolbar,
+    RouterOutlet,
+    MatNavList,
+    MatListItem,
+    RouterLink,
+    MatSidenav,
+    MatSidenavContent,
+    MatIconButton,
+  ],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss'
+})
+export class MainLayoutComponent {
+  protected readonly sidenavLinks: readonly SidenavLinkModel[] = [
+    {
+      routerLink: ['/', 'admin', 'ingredients'],
+      icon: 'food_bank',
+      label: 'Інгредієнти'
+    },
+    {
+      routerLink: ['/', 'admin', 'meals'],
+      icon: 'lunch_dining',
+      label: 'Страви'
+    },
+  ];
+
+  protected $shouldShowSidenav = computed(() => {
+    return this.sidenavLinks.length !== 0;
+  });
+
+  protected $isSidenavFixed = signal<boolean>(false);
+  protected $isSidenavExpanded = signal<boolean>(false);
+
+  protected onMenuButtonClick() {
+    this.$isSidenavFixed.update(val => !val);
+    this.$isSidenavExpanded.update(val => !val);
+  }
+
+  protected onSidenavMouseEnter() {
+    this.changeSidenavExpandedState(true);
+  }
+
+  protected onSidenavMouseLeave() {
+    this.changeSidenavExpandedState(false);
+  }
+
+  private changeSidenavExpandedState(isExpanded: boolean) {
+    if (!this.$isSidenavFixed()) {
+      this.$isSidenavExpanded.set(isExpanded);
+    }
+  }
+}
