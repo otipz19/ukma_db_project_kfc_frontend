0754032b32c90d54759f4e7fd48452b0bfa17ee9
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-04-07 20:29:30 +0300

    Fix api basepath

--- src/app/core/config/api-config.ts
+++ src/app/core/config/api-config.ts
@@ -4,7 +4,7 @@ import {importProvidersFrom} from "@angular/core";
 export class ApiConfig extends Configuration {
   constructor() {
     super({
-      basePath: 'localhost:8080'
+      basePath: 'http://localhost:8080/KFC',
     });
   }
 }
--- src/app/core/services/sidenav-links.service.ts
+++ src/app/core/services/sidenav-links.service.ts
@@ -21,7 +21,8 @@ export class SidenavLinksService {
   private readonly roleToAllowedLinks = new Map<UserRoleState, Array<LinkAlias>>([
     ['ADMIN', ['ingredients', 'meals']],
     // TODO: Remove when auth is finished
-    ['UNAUTHENTICATED', ['ingredients', 'meals']]
+    ['UNAUTHENTICATED', ['ingredients', 'meals']],
+    ['CLIENT', ['ingredients', 'meals']]
   ]);
 
   private readonly allLinks = new Map<LinkAlias, SidenavLinkModel>([
