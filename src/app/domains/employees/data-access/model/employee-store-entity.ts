f0b20819ef96b26963e4e9ce3d54cea44040b56c
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-04-14 19:49:23 +0300

    Fix Employee:birthDate type format bug

--- src/app/domains/employees/data-access/model/employee-store-entity.ts
+++ src/app/domains/employees/data-access/model/employee-store-entity.ts
@@ -1,8 +1,16 @@
 import {Employee} from "../../../../api";
 
-export type EmployeeStoreEntity = Omit<Employee, 'userId'> & {id: number};
+export type EmployeeStoreEntity = Omit<Employee, 'userId'> & { id: number };
 
 export function mapEmployeeToStoreEntity(employee: Employee): EmployeeStoreEntity {
-  const {userId, ...rest} = employee;
-  return {id: userId, ...rest};
+  let {userId, birthDate, ...rest} = employee;
+  // This is a workaround because for some reason backend actually sends [year, month, day] not a string
+  birthDate = mapDateFromArrayResponse(birthDate as any);
+  return {id: userId, birthDate, ...rest};
+}
+
+function mapDateFromArrayResponse(arrDate: [number, number, number]): string {
+  const dateObj = new Date(arrDate[0], arrDate[1] - 1, arrDate[2]);
+  // Format it as a string (e.g., YYYY-MM-DD)
+  return dateObj.toISOString().split('T')[0];
 }
