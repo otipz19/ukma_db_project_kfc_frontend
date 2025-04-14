f9e969391ebb942bf3fb602a057aedc5d0d0937c
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-04-14 16:09:38 +0300

    Add icons

--- src/app/domains/employees/view/select-models/employee-position-select-model.ts
+++ src/app/domains/employees/view/select-models/employee-position-select-model.ts
@@ -1,16 +1,7 @@
 import {EmployeePosition} from "../../../../api";
 import {
-  optionsToSelectOptionModelList,
-  SelectOptionLabelsMap,
   SelectOptionModel
 } from "../../../../shared/form/utils/select-options";
-
-export const EmployeePositionSelectLabels: SelectOptionLabelsMap<EmployeePosition> = {
-  'MANAGER': 'Менеджер',
-  'TOP_MANAGER': 'Топ менеджер',
-  'COOK': 'Кухар',
-  'CASHIER': 'Касир'
-};
 
 export const ManagerPositionSelectOptions: SelectOptionModel<EmployeePosition>[] = [
   {value: EmployeePosition.MANAGER, label: 'Менеджер'}
