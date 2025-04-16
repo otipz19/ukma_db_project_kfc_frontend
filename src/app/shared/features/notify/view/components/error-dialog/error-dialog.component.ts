e81136debaae6d8fbc6161e5c3291f34394158df
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-04-17 01:31:57 +0300

    Update ErrorDialogComponent

--- src/app/shared/features/notify/view/components/error-dialog/error-dialog.component.ts
+++ src/app/shared/features/notify/view/components/error-dialog/error-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -27,18 +27,35 @@ export type ErrorDialogConfig = {
   styleUrl: './error-dialog.component.scss'
 })
 export class ErrorDialogComponent implements OnInit {
+  private static readonly DEFAULT_MSG = 'Сталася помилка...';
+
   private readonly dialogRef = inject(MatDialogRef<ErrorDialogComponent>);
   protected readonly config: ErrorDialogConfig | undefined = inject(MAT_DIALOG_DATA);
 
-  protected errorMessage = 'Сталася помилка...';
+  protected readonly $errorMessage = signal<string>(ErrorDialogComponent.DEFAULT_MSG);
 
   ngOnInit() {
     const err = this.config?.error;
-    if(err instanceof HttpErrorResponse) {
-      this.errorMessage = err.error;
+    if (err instanceof HttpErrorResponse) {
+      this.$errorMessage.set(this.getHttpErrorMessage(err));
     } else {
-      this.errorMessage = typeof err === 'string' ? err : JSON.stringify(err);
+      this.$errorMessage.set(this.getStringOrObject(err));
     }
+  }
+
+  private getHttpErrorMessage(response: HttpErrorResponse): string {
+    const error = response.error;
+    if (error.constructor === Array) {
+      if (error.length === 0) {
+        return ErrorDialogComponent.DEFAULT_MSG;
+      }
+      return this.getStringOrObject(error[0] as any);
+    }
+    return this.getStringOrObject(error);
+  }
+
+  private getStringOrObject(value: string | object): string {
+    return typeof value === 'string' ? value : JSON.stringify(value);
   }
 
   protected onClose(): void {
