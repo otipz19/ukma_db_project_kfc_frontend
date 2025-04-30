15b69e54314e039ecca8bf62fbc5eddace8097aa
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-04-30 20:56:14 +0300

    Update OrderStateService to store state in localStorage separated for different users

--- src/app/domains/orders/features/create-order/data-access/services/order-state.service.ts
+++ src/app/domains/orders/features/create-order/data-access/services/order-state.service.ts
@@ -1,21 +1,36 @@
-import {Injectable} from "@angular/core";
+import {computed, inject, Injectable} from "@angular/core";
 import {OrderMeal} from "../types/order-meal";
+import {AuthService} from "../../../../../../core/services/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderStateService {
-  private meals: OrderMeal[] = [];
+  private static LS_KEY_SUFFIX = "KFC_ORDER_STATE";
+
+  private readonly authService = inject(AuthService);
+
+  private readonly $lsKey = computed(() => {
+    const user = this.authService.$currentUser();
+    if (!user) {
+      return '';
+    }
+    return user.id + OrderStateService.LS_KEY_SUFFIX;
+  })
 
   saveOrderState(meals: OrderMeal[]) {
-    this.meals = [...meals];
+    localStorage.setItem(this.$lsKey(), JSON.stringify(meals));
   }
 
   clearOrderState() {
-    this.meals = [];
+    localStorage.removeItem(this.$lsKey());
   }
 
-  getOrderState() {
-    return [...this.meals];
+  getOrderState(): OrderMeal[] {
+    const fromLs = localStorage.getItem(this.$lsKey());
+    if(!fromLs) {
+      return [];
+    }
+    return JSON.parse(fromLs);
   }
 }
