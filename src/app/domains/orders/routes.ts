4184f9205dd2744733540241ed872b699956f94e
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-04-30 20:44:11 +0300

    Fix CurrentRestaurantService

--- src/app/domains/orders/routes.ts
+++ src/app/domains/orders/routes.ts
@@ -1,14 +1,16 @@
 import {Routes} from "@angular/router";
 import {hasRoleRouteGuard} from "../../core/route-guards/has-role-route.guard";
 import {UserRole} from "../../api";
+import {authenticatedRouteGuard} from "../../core/route-guards/authenticated.route-guard";
 
 export const ORDERS_ROUTES: Routes = [
   {
     path: 'orders',
+    canActivate: [authenticatedRouteGuard],
     children: [
       {
         path: 'create',
-        canActivate: [hasRoleRouteGuard(UserRole.ADMIN, UserRole.CLIENT, UserRole.CASHIER, UserRole.MANAGER)],
+        canActivate: [hasRoleRouteGuard(UserRole.CLIENT, UserRole.CASHIER, UserRole.MANAGER)],
         loadComponent: () => import('./view/pages/create-order-page/create-order-page.component').then(r => r.CreateOrderPageComponent)
       }
     ]
--- src/app/domains/restaurants/features/current-restaurant/data-access/services/current-restaurant.service.ts
+++ src/app/domains/restaurants/features/current-restaurant/data-access/services/current-restaurant.service.ts
@@ -1,16 +1,21 @@
 import {inject, Injectable, signal} from "@angular/core";
 import {Restaurant} from "../../../../../../api/model/restaurant";
-import {EMPTY, Observable, of, switchMap} from "rxjs";
+import {EMPTY, Observable, of, switchMap, tap} from "rxjs";
 import {MatDialog} from "@angular/material/dialog";
 import {
   SelectRestaurantDialogComponent
 } from "../../view/components/select-restaurant-dialog/select-restaurant-dialog.component";
+import {AuthService} from "../../../../../../core/services/auth.service";
+import {RestaurantControllerService} from "../../../../../../api/api/restaurantController.service";
+import {UserRole} from "../../../../../../api";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentRestaurantService {
   private readonly matDialog = inject(MatDialog);
+  private readonly authService = inject(AuthService);
+  private readonly restaurantApi = inject(RestaurantControllerService);
 
   private readonly $currentRestaurant = signal<Restaurant | undefined>(undefined);
 
@@ -19,7 +24,20 @@ export class CurrentRestaurantService {
     if (saved) {
       return of(saved);
     }
-    return this.selectCurrentRestaurant$();
+
+    return this.authService.$isEmployee() && !this.authService.hasRole(UserRole.ADMIN)
+      ? this.setEmployeeRestaurant$()
+      : this.selectCurrentRestaurant$();
+  }
+
+  private setEmployeeRestaurant$(): Observable<Restaurant> {
+    const id = this.authService.$currentEmployee()!.restaurantId!;
+    return this.restaurantApi.getRestaurantById(id)
+      .pipe(
+        tap(restaurant => {
+          this.$currentRestaurant.set(restaurant);
+        })
+      );
   }
 
   selectCurrentRestaurant$(): Observable<Restaurant> {
@@ -34,10 +52,13 @@ export class CurrentRestaurantService {
     return dialogRef.afterClosed()
       .pipe(
         switchMap(restaurant => {
-          if(restaurant) {
+          if (restaurant) {
             return of(restaurant);
           }
           return EMPTY;
+        }),
+        tap(restaurant => {
+          this.$currentRestaurant.set(restaurant);
         })
       );
   }
