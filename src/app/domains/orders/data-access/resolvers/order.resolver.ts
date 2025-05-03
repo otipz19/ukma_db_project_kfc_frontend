c0b1f4ec41ac1a0928760625e9cb6ad42a7281cd
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-05-03 05:11:32 +0300

    Fix timestamp view on order profile page

--- src/app/domains/orders/data-access/resolvers/order.resolver.ts
+++ src/app/domains/orders/data-access/resolvers/order.resolver.ts
@@ -2,8 +2,9 @@ import {RedirectCommand, ResolveFn, Router} from "@angular/router";
 import {Order} from "../../../../api/model/order";
 import {inject} from "@angular/core";
 import {OrderControllerService} from "../../../../api/api/orderController.service";
-import {catchError, EMPTY} from "rxjs";
+import {catchError, EMPTY, map, tap} from "rxjs";
 import {HttpErrorResponse} from "@angular/common/http";
+import {convertUnixTimestampToDateString} from "../../../../shared/utils/convert-unix-timestamp-to-date-string";
 
 export const ORDER_RESOLVER_KEY = 'ORDER_RESOLVER_KEY';
 
@@ -19,6 +20,9 @@ export const orderResolver: ResolveFn<Order> = (route, state) => {
 
   return orderApi.getOrderById(orderId)
     .pipe(
+      tap(order => {
+        order.dateCreated = convertUnixTimestampToDateString(order.dateCreated);
+      }),
       catchError(err => {
         if (err instanceof HttpErrorResponse && err.status === 403) {
           router.navigate(['/', 'forbidden']);
